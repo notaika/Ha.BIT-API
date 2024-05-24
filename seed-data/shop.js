eb5f203f0359dd2909d18220383cc2d8d17f4dcb
notaika <ercamanalo@gmail.com>
2024-05-24 09:05:48 -0700

    initial folder and file structure setup

---
+++ seed-data/shop.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Long Sword',
+        image: '/public/images/long-sword.png',
+        description: 'A rusty iron sword. Normal attack speed',
+        price: 20,
+        attack: 3,
+    },
+    {
+        id: 2,
+        name: 'Double-handed Sword',
+        image: '/public/images/double-handed-sword.png',
+        description: 'A dull iron double-handed sword. Slow attack speed.',
+        price: 20,
+        attack: 5,
+    },
+    {
+        id: 3,
+        name: 'Dagger',
+        image: 'public/images/dagger.png',
+        description: 'Is this a fruit knife? Fast attack speed.',
+        price: 20,
+        attack: 1,
+    },
+]
---
+++ seed-data/users.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        id: 1,
+        username: 'Admin',
+        password: 'agoodpassword',
+    },
+    {
+        id: 2,
+        username: 'Test User',
+        password: 'testpassword'
+    },
+]
